c307bf561400c2c420845bde34bb4664f95b5dbe
weishu <twsxtd@gmail.com>
2025-11-08 08:31:02 +0000

    kernel: remove ksu_compat_{open,read,write} because we're in the right context now
    Co-authored-by: weishu <twsxtd@gmail.com>

--- kernel/kernel_compat.h
+++ kernel/kernel_compat.h
@@ -4,30 +4,6 @@
 #include <linux/fs.h>
 #include <linux/version.h>
 #include <linux/cred.h>
-#include "ss/policydb.h"
-#include "linux/key.h"
-
-/*
- * Linux 6.8+ does not have LKM support, due to numerous changes on LSM.
- * Let's fails if MODULE were defined.
- */
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 8, 0) && defined(MODULE) 
-#error "LKM mode is not supported on Linux 6.8+, aborting build."
-#endif
-
-/*
- * Adapt to Huawei HISI kernel without affecting other kernels ,
- * Huawei Hisi Kernel EBITMAP Enable or Disable Flag ,
- * From ss/ebitmap.h
- */
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0)) &&                           \
-        (LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0)) ||               \
-    (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 14, 0)) &&                      \
-        (LINUX_VERSION_CODE < KERNEL_VERSION(4, 15, 0))
-#ifdef HISI_SELINUX_EBITMAP_RO
-#define CONFIG_IS_HW_HISI
-#endif
-#endif
 
 // Checks for UH, KDP and RKP
 #ifdef SAMSUNG_UH_DRIVER_EXIST
@@ -48,15 +24,6 @@ extern long ksu_strncpy_from_user_retry(char *dst,
     defined(CONFIG_KSU_ALLOWLIST_WORKAROUND)
 extern struct key *init_session_keyring;
 #endif
-
-extern void ksu_android_ns_fs_check(void);
-extern struct file *ksu_filp_open_compat(const char *filename, int flags,
-                     umode_t mode);
-extern ssize_t ksu_kernel_read_compat(struct file *p, void *buf, size_t count,
-                      loff_t *pos);
-extern ssize_t ksu_kernel_write_compat(struct file *p, const void *buf,
-                       size_t count, loff_t *pos);
-extern long ksu_copy_from_user_nofault(void *dst, const void __user *src, size_t size);
 /*
  * ksu_copy_from_user_retry
  * try nofault copy first, if it fails, try with plain
