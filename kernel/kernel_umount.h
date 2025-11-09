66f37c7310dbb2875f544c164c6287c4861818f8
ShirkNeko <109797057+ShirkNeko@users.noreply.github.com>
2025-11-09 21:39:04 +0800

    Revert "kernel: Apply the SUSFS patch"
    This reverts commit ad0f3f60258cbfeda31be310a096cf1ddbb94ab6.

--- kernel/kernel_umount.h
+++ kernel/kernel_umount.h
@@ -6,10 +6,6 @@
 void ksu_kernel_umount_init(void);
 void ksu_kernel_umount_exit(void);
 
-#ifdef CONFIG_KSU_SUSFS_TRY_UMOUNT
-void susfs_try_umount_all(uid_t uid);
-#endif
-
 // Handler function to be called from setresuid hook
 int ksu_handle_umount(uid_t old_uid, uid_t new_uid);
 
--- kernel/sucompat.c
+++ kernel/sucompat.c
@@ -12,9 +12,6 @@
 #else
 #include <linux/sched.h>
 #endif
-#ifdef CONFIG_KSU_SUSFS_SUS_SU
-#include <linux/susfs_def.h>
-#endif
 
 #include "allowlist.h"
 #include "feature.h"
@@ -25,7 +22,6 @@
 #include "syscall_hook_manager.h"
 
 #include "sulog.h"
-#include "kernel_compat.h"
 
 #define SU_PATH "/system/bin/su"
 #define SH_PATH "/system/bin/sh"
@@ -41,10 +37,8 @@ static int su_compat_feature_get(u64 *value)
 static int su_compat_feature_set(u64 value)
 {
     bool enable = value != 0;
-
     ksu_su_compat_enabled = enable;
     pr_info("su_compat: set to %d\n", enable);
-
     return 0;
 }
 
@@ -55,13 +49,7 @@ static const struct ksu_feature_handler su_compat_handler = {
     .set_handler = su_compat_feature_set,
 };
 
-static const char sh_path[] = "/system/bin/sh";
-static const char ksud_path[] = KSUD_PATH;
-static const char su[] = SU_PATH;
-
-bool ksu_sucompat_hook_state __read_mostly = true;
-
-static inline void __user *userspace_stack_buffer(const void *d, size_t len)
+static void __user *userspace_stack_buffer(const void *d, size_t len)
 {
     // To avoid having to mmap a page in userspace, just write below the stack
     // pointer.
@@ -70,19 +58,24 @@ static inline void __user *userspace_stack_buffer(const void *d, size_t len)
     return copy_to_user(p, d, len) ? NULL : p;
 }
 
-static inline char __user *sh_user_path(void)
+static char __user *sh_user_path(void)
 {
+    static const char sh_path[] = "/system/bin/sh";
+
     return userspace_stack_buffer(sh_path, sizeof(sh_path));
 }
 
-static inline char __user *ksud_user_path(void)
+static char __user *ksud_user_path(void)
 {
+    static const char ksud_path[] = KSUD_PATH;
+
     return userspace_stack_buffer(ksud_path, sizeof(ksud_path));
 }
 
 int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
              int *__unused_flags)
 {
+    const char su[] = SU_PATH;
 
 #ifdef KSU_MANUAL_HOOK
     if (!ksu_su_compat_enabled) {
@@ -90,19 +83,13 @@ int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
     }
 #endif
 
-#ifndef CONFIG_KSU_SUSFS_SUS_SU
     if (!ksu_is_allow_uid_for_current(current_uid().val)) {
         return 0;
     }
-#endif
 
-#ifdef CONFIG_KSU_SUSFS_SUS_SU
-    char path[sizeof(su) + 1] = {0};
-#else
     char path[sizeof(su) + 1];
     memset(path, 0, sizeof(path));
-#endif
-    ksu_strncpy_from_user_nofault(path, *filename_user, sizeof(path));
+    strncpy_from_user_nofault(path, *filename_user, sizeof(path));
 
     if (unlikely(!memcmp(path, su, sizeof(su)))) {
 #if __SULOG_GATE
@@ -115,53 +102,26 @@ int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
     return 0;
 }
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 0) && defined(CONFIG_KSU_SUSFS_SUS_SU)
-struct filename* susfs_ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags) {
-    struct filename *name = getname_flags(*filename_user, getname_statx_lookup_flags(*flags), NULL);
-
-    if (unlikely(IS_ERR(name) || name->name == NULL)) {
-        return name;
-    }
-
-    if (likely(memcmp(name->name, su, sizeof(su)))) {
-        return name;
-    }
-
-    const char sh[] = SH_PATH;
-#if __SULOG_GATE
-    ksu_sulog_report_syscall(current_uid().val, NULL, "vfs_fstatat", sh);
-#endif
-    pr_info("vfs_fstatat su->sh!\n");
-    memcpy((void *)name->name, sh, sizeof(sh));
-    return name;
-}
-#endif
-
 int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags)
 {
+    // const char sh[] = SH_PATH;
+    const char su[] = SU_PATH;
 
 #ifdef KSU_MANUAL_HOOK
     if (!ksu_su_compat_enabled) {
         return 0;
     }
 #endif
-
-#ifndef CONFIG_KSU_SUSFS_SUS_SU
     if (!ksu_is_allow_uid_for_current(current_uid().val)) {
         return 0;
     }
-#endif
 
     if (unlikely(!filename_user)) {
         return 0;
     }
 
-#ifdef CONFIG_KSU_SUSFS_SUS_SU
-    char path[sizeof(su) + 1] = {0};
-#else
     char path[sizeof(su) + 1];
     memset(path, 0, sizeof(path));
-#endif
 // Remove this later!! we use syscall hook, so this will never happen!!!!!
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 18, 0) && 0
     // it becomes a `struct filename *` after 5.18
@@ -176,7 +136,7 @@ int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags)
     pr_info("vfs_statx su->sh!\n");
     memcpy((void *)filename->name, sh, sizeof(sh));
 #else
-    ksu_strncpy_from_user_nofault(path, *filename_user, sizeof(path));
+    strncpy_from_user_nofault(path, *filename_user, sizeof(path));
 
     if (unlikely(!memcmp(path, su, sizeof(su)))) {
 #if __SULOG_GATE
@@ -194,46 +154,30 @@ int ksu_handle_execve_sucompat(const char __user **filename_user,
                    void *__never_use_argv, void *__never_use_envp,
                    int *__never_use_flags)
 {
-    //const char su[] = SU_PATH;
-#ifdef CONFIG_KSU_SUSFS_SUS_SU
-    char path[sizeof(su) + 1] = {0};
-#else
+    const char su[] = SU_PATH;
     char path[sizeof(su) + 1];
-#endif
 
 #ifdef KSU_MANUAL_HOOK
     if (!ksu_su_compat_enabled){
         return 0;
     }
 #endif
-
     if (unlikely(!filename_user))
         return 0;
 
-    /*
-     * nofault variant fails silently due to pagefault_disable
-     * some cpus dont really have that good speculative execution
-     * access_ok to substitute set_fs, we check if pointer is accessible
-     */
-    if (!ksu_access_ok(*filename_user, sizeof(path)))
-        return 0;
-
-    // success = returns number of bytes and should be less than path
-    long len = strncpy_from_user(path, *filename_user, sizeof(path));
-    if (len <= 0 || len > sizeof(path))
-        return 0;
-    // strncpy_from_user_nofault does this too
-    path[sizeof(path) - 1] = '\0';
+    memset(path, 0, sizeof(path));
+    strncpy_from_user_nofault(path, *filename_user, sizeof(path));
 
     if (likely(memcmp(path, su, sizeof(su))))
         return 0;
 
 #if __SULOG_GATE
-    ksu_sulog_report_syscall(current_uid().val, NULL, "execve", path);
     bool is_allowed = ksu_is_allow_uid_for_current(current_uid().val);
+    ksu_sulog_report_syscall(current_uid().val, NULL, "execve", path);
+    
     if (!is_allowed)
         return 0;
-    
+
     ksu_sulog_report_su_attempt(current_uid().val, NULL, path, is_allowed);
 #else
     if (!ksu_is_allow_uid_for_current(current_uid().val)) {
