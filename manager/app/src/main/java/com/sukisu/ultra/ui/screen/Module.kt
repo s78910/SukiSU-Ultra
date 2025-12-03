tiann/KernelSU

manager: show changelog from module repos

manager/app/src/main/java/me/weishu/kernelsu/ui/screen/Module.kt
@@ -113,6 +113,8 @@ import me.weishu.kernelsu.ui.util.DownloadListener
 import me.weishu.kernelsu.ui.util.download
 import me.weishu.kernelsu.ui.util.getFileName
 import me.weishu.kernelsu.ui.util.hasMagisk
+import me.weishu.kernelsu.ui.util.module.fetchModuleDetail
+import me.weishu.kernelsu.ui.util.module.fetchReleaseDescriptionHtml
 import me.weishu.kernelsu.ui.util.toggleModule
 import me.weishu.kernelsu.ui.util.undoUninstallModule
 import me.weishu.kernelsu.ui.util.uninstallModule
@@ -259,11 +261,27 @@ fun ModulePager(
         }
 
         val changelog = changelogResult.getOrElse { "" }
+        var htmlLog = ""
+        if (changelog.isBlank()) {
+            withContext(Dispatchers.IO) {
+                runCatching {
+                    val detail = fetchModuleDetail(module.id)
+                    val latestTag = detail?.latestTag ?: ""
+                    val html = if (latestTag.isNotBlank()) fetchReleaseDescriptionHtml(module.id, latestTag) else null
+                    if (html != null) htmlLog = html
+                }
+            }
+        }
 
         val confirmResult = confirmDialog.awaitConfirm(
-            if (changelog.isNotEmpty()) changelogText else updateText,
-            content = changelog.ifBlank { startDownloadingText.format(module.name) },
-            markdown = changelog.isNotBlank(),
+            if (changelog.isNotEmpty() || htmlLog.isNotEmpty()) changelogText else updateText,
+            content = when {
+                changelog.isNotEmpty() -> changelog
+                htmlLog.isNotEmpty() -> htmlLog
+                else -> startDownloadingText.format(module.name)
+            },
+            markdown = changelog.isNotEmpty(),
+            html = htmlLog.isNotEmpty(),
             confirm = updateText,
         )
 

manager/app/src/main/java/me/weishu/kernelsu/ui/util/module/ModuleRepoApi.kt
@@ -1,0 +1,146 @@
+package me.weishu.kernelsu.ui.util.module
+
+import me.weishu.kernelsu.ksuApp
+import okhttp3.Request
+import org.json.JSONArray
+import org.json.JSONObject
+
+data class RepoSummary(
+    val latestVersion: String,
+    val versionCode: Int,
+    val downloadUrl: String
+)
+
+fun sanitizeVersionString(version: String): String {
+    return version.replace(Regex("[^a-zA-Z0-9.\\-_]"), "_")
+}
+
+fun fetchRepoIndex(): Map<String, RepoSummary> {
+    val url = "https://modules.kernelsu.org/modules.json"
+    ksuApp.okhttpClient.newCall(Request.Builder().url(url).build()).execute().use { resp ->
+        if (!resp.isSuccessful) return emptyMap()
+        val body = resp.body?.string() ?: return emptyMap()
+        val arr = JSONArray(body)
+        val result = mutableMapOf<String, RepoSummary>()
+        for (idx in 0 until arr.length()) {
+            val obj = arr.optJSONObject(idx) ?: continue
+            val id = obj.optString("moduleId", "").ifBlank { continue }
+            val lr = obj.optJSONObject("latestRelease") ?: continue
+            val ver = sanitizeVersionString(lr.optString("name", lr.optString("version", "")))
+            val vcode = lr.optInt("versionCode", 0)
+            val dl = lr.optString("downloadUrl", "")
+            if (vcode > 0 && dl.isNotBlank()) {
+                result[id] = RepoSummary(ver, vcode, dl)
+            }
+        }
+        return result
+    }
+}
+
+data class ModuleDetail(
+    val readme: String?,
+    val latestTag: String,
+    val latestTime: String,
+    val latestAssetName: String?,
+    val latestAssetUrl: String?,
+    val releases: List<ReleaseInfo>
+)
+
+data class ReleaseInfo(
+    val name: String,
+    val tagName: String,
+    val publishedAt: String,
+    val descriptionHTML: String,
+    val assets: List<ReleaseAssetInfo>
+)
+
+data class ReleaseAssetInfo(
+    val name: String,
+    val downloadUrl: String,
+    val size: Long
+)
+
+fun fetchReleaseDescriptionHtml(moduleId: String, latestTag: String): String? {
+    val url = "https://modules.kernelsu.org/module/$moduleId.json"
+    ksuApp.okhttpClient.newCall(Request.Builder().url(url).build()).execute().use { resp ->
+        if (!resp.isSuccessful) return null
+        val body = resp.body?.string() ?: return null
+        val obj = JSONObject(body)
+        val releasesArray = obj.optJSONArray("releases") ?: return null
+        var fallbackHtml: String? = null
+        for (i in 0 until releasesArray.length()) {
+            val r = releasesArray.optJSONObject(i) ?: continue
+            val descHtml = r.optString("descriptionHTML", "")
+            if (fallbackHtml == null && descHtml.isNotBlank()) {
+                fallbackHtml = descHtml
+            }
+            val rname = r.optString("name", r.optString("tagName", r.optString("version", "")))
+            if (rname == latestTag && descHtml.isNotBlank()) {
+                return descHtml
+            }
+        }
+        return fallbackHtml
+    }
+}
+
+fun fetchModuleDetail(moduleId: String): ModuleDetail? {
+    val url = "https://modules.kernelsu.org/module/$moduleId.json"
+    ksuApp.okhttpClient.newCall(Request.Builder().url(url).build()).execute().use { resp ->
+        if (!resp.isSuccessful) return null
+        val body = resp.body?.string() ?: return null
+        val obj = JSONObject(body)
+
+        val readme = obj.optString("readme", "").ifBlank { null }
+    val lr = obj.optJSONObject("latestRelease")
+    var latestTag = ""
+    var latestTime = ""
+    var latestAssetName: String? = null
+    var latestAssetUrl: String? = null
+    if (lr != null) {
+        latestTag = lr.optString("name", lr.optString("version", ""))
+        latestTime = lr.optString("time", "")
+        val urlDl = lr.optString("downloadUrl", "")
+        if (urlDl.isNotEmpty()) {
+            latestAssetName = urlDl.substringAfterLast('/')
+            latestAssetUrl = urlDl
+        }
+    } else {
+        // fallback: schema may provide latest release as a string
+        latestTag = obj.optString("latestRelease", "")
+    }
+
+        val releasesArray = obj.optJSONArray("releases")
+        val releases = if (releasesArray != null) {
+            (0 until releasesArray.length()).mapNotNull { rIdx ->
+                val r = releasesArray.optJSONObject(rIdx) ?: return@mapNotNull null
+                val rname = r.optString("name", "")
+                val publishedAt = r.optString("publishedAt", "")
+                val descHtml = r.optString("descriptionHTML", "")
+                val assetsArray = r.optJSONArray("releaseAssets") ?: JSONArray()
+                val assets = (0 until assetsArray.length()).mapNotNull { aIdx ->
+                    val a = assetsArray.optJSONObject(aIdx) ?: return@mapNotNull null
+                    val aname = a.optString("name", "")
+                    val adl = a.optString("downloadUrl", "")
+                    val asz = a.optLong("size", 0L)
+                    if (aname.isEmpty() || adl.isEmpty()) null else ReleaseAssetInfo(aname, adl, asz)
+                }
+                ReleaseInfo(
+                    name = rname,
+                    tagName = rname,
+                    publishedAt = publishedAt,
+                    descriptionHTML = descHtml,
+                    assets = assets
+                )
+            }
+        } else emptyList()
+
+        return ModuleDetail(
+            readme = readme,
+            latestTag = latestTag,
+            latestTime = latestTime,
+            latestAssetName = latestAssetName,
+            latestAssetUrl = latestAssetUrl,
+            releases = releases
+        )
+    }
+}

manager/app/src/main/java/me/weishu/kernelsu/ui/util/module/VersionCompare.kt
@@ -1,0 +1,20 @@
+package me.weishu.kernelsu.ui.util.module
+
+enum class UpdateState {
+    CAN_UPDATE,
+    EQUAL,
+    OLDER,
+    NOT_INSTALLED,
+    UNKNOWN
+}
+
+fun compareVersionCode(installedCode: Int?, repoCode: Int): UpdateState {
+    if (repoCode <= 0) return UpdateState.UNKNOWN
+    val ic = installedCode ?: return UpdateState.NOT_INSTALLED
+    return when {
+        repoCode > ic -> UpdateState.CAN_UPDATE
+        repoCode == ic -> UpdateState.EQUAL
+        else -> UpdateState.OLDER
+    }
+}
+

manager/app/src/main/java/me/weishu/kernelsu/ui/viewmodel/ModuleViewModel.kt
@@ -24,6 +24,9 @@ import me.weishu.kernelsu.ksuApp
 import me.weishu.kernelsu.ui.component.SearchStatus
 import me.weishu.kernelsu.ui.util.HanziToPinyin
 import me.weishu.kernelsu.ui.util.listModules
+import me.weishu.kernelsu.ui.util.module.RepoSummary
+import me.weishu.kernelsu.ui.util.module.fetchRepoIndex
+import me.weishu.kernelsu.ui.util.module.sanitizeVersionString
 import org.json.JSONArray
 import org.json.JSONObject
 import java.text.Collator
@@ -157,6 +160,7 @@ class ModuleViewModel : ViewModel() {
                         executable -> 3
                         else -> 4
                     }
+
                     sortEnabledFirst && !sortActionFirst -> if (it.enabled) 1 else 2
                     !sortEnabledFirst && sortActionFirst -> if (executable) 1 else 2
                     else -> 1
@@ -226,49 +230,16 @@ class ModuleViewModel : ViewModel() {
         }
     }
 
-    @Immutable
-    data class RepoSummary(
-        val latestVersion: String,
-        val versionCode: Int,
-        val downloadUrl: String
-    )
-
     private val _repoIndex = mutableStateMapOf<String, RepoSummary>()
-    val repoIndex: SnapshotStateMap<String, RepoSummary> = _repoIndex
 
     suspend fun refreshRepoIndex() {
-        val parsed = withContext(Dispatchers.IO) {
-            kotlin.runCatching {
-                val req = okhttp3.Request.Builder().url("https://modules.kernelsu.org/modules.json").build()
-                ksuApp.okhttpClient.newCall(req).execute().use { resp ->
-                    if (!resp.isSuccessful) emptyList() else {
-                        val body = resp.body?.string() ?: return@use emptyList()
-                        val arr = org.json.JSONArray(body)
-                        (0 until arr.length()).mapNotNull { idx ->
-                            val obj = arr.optJSONObject(idx) ?: return@mapNotNull null
-                            val id = obj.optString("moduleId", "").ifBlank { return@mapNotNull null }
-                            val lr = obj.optJSONObject("latestRelease")
-                            if (lr == null) null else {
-                                val ver = sanitizeVersionString(lr.optString("name", lr.optString("version", "")))
-                                val vcode = lr.optInt("versionCode", 0)
-                                val dl = lr.optString("downloadUrl", "")
-                                if (vcode <= 0 || dl.isBlank()) null else id to RepoSummary(ver, vcode, dl)
-                            }
-                        }
-                    }
-                }
-            }.getOrElse { emptyList() }
-        }
-
+        val parsed = withContext(Dispatchers.IO) { fetchRepoIndex() }
         withContext(Dispatchers.Main) {
             _repoIndex.clear()
             parsed.forEach { (id, summary) -> _repoIndex[id] = summary }
         }
     }
 
-    private fun sanitizeVersionString(version: String): String {
-        return version.replace(Regex("[^a-zA-Z0-9.\\-_]"), "_")
-    }
 
     private fun ModuleInfo.toSignature(): ModuleUpdateSignature {
         return ModuleUpdateSignature(

s78910/SukiSU-Ultra

manager/app/src/main/java/com/sukisu/ultra/ui/screen/Module.kt
@@ -113,6 +113,8 @@ import com.sukisu.ultra.ui.util.DownloadListener
 import com.sukisu.ultra.ui.util.download
 import com.sukisu.ultra.ui.util.getFileName
 import com.sukisu.ultra.ui.util.hasMagisk
+import com.sukisu.ultra.ui.util.module.fetchModuleDetail
+import com.sukisu.ultra.ui.util.module.fetchReleaseDescriptionHtml
 import com.sukisu.ultra.ui.util.toggleModule
 import com.sukisu.ultra.ui.util.undoUninstallModule
 import com.sukisu.ultra.ui.util.uninstallModule
@@ -259,11 +261,27 @@ fun ModulePager(
         }
 
         val changelog = changelogResult.getOrElse { "" }
+        var htmlLog = ""
+        if (changelog.isBlank()) {
+            withContext(Dispatchers.IO) {
+                runCatching {
+                    val detail = fetchModuleDetail(module.id)
+                    val latestTag = detail?.latestTag ?: ""
+                    val html = if (latestTag.isNotBlank()) fetchReleaseDescriptionHtml(module.id, latestTag) else null
+                    if (html != null) htmlLog = html
+                }
+            }
+        }
 
         val confirmResult = confirmDialog.awaitConfirm(
-            if (changelog.isNotEmpty()) changelogText else updateText,
-            content = changelog.ifBlank { startDownloadingText.format(module.name) },
-            markdown = changelog.isNotBlank(),
+            if (changelog.isNotEmpty() || htmlLog.isNotEmpty()) changelogText else updateText,
+            content = when {
+                changelog.isNotEmpty() -> changelog
+                htmlLog.isNotEmpty() -> htmlLog
+                else -> startDownloadingText.format(module.name)
+            },
+            markdown = changelog.isNotEmpty(),
+            html = htmlLog.isNotEmpty(),
             confirm = updateText,
         )
 

manager/app/src/main/java/com/sukisu/ultra/ui/util/module/ModuleRepoApi.kt
@@ -1,0 +1,146 @@
+package com.sukisu.ultra.ui.util.module
+
+import com.sukisu.ultra.ksuApp
+import okhttp3.Request
+import org.json.JSONArray
+import org.json.JSONObject
+
+data class RepoSummary(
+    val latestVersion: String,
+    val versionCode: Int,
+    val downloadUrl: String
+)
+
+fun sanitizeVersionString(version: String): String {
+    return version.replace(Regex("[^a-zA-Z0-9.\\-_]"), "_")
+}
+
+fun fetchRepoIndex(): Map<String, RepoSummary> {
+    val url = "https://modules.kernelsu.org/modules.json"
+    ksuApp.okhttpClient.newCall(Request.Builder().url(url).build()).execute().use { resp ->
+        if (!resp.isSuccessful) return emptyMap()
+        val body = resp.body?.string() ?: return emptyMap()
+        val arr = JSONArray(body)
+        val result = mutableMapOf<String, RepoSummary>()
+        for (idx in 0 until arr.length()) {
+            val obj = arr.optJSONObject(idx) ?: continue
+            val id = obj.optString("moduleId", "").ifBlank { continue }
+            val lr = obj.optJSONObject("latestRelease") ?: continue
+            val ver = sanitizeVersionString(lr.optString("name", lr.optString("version", "")))
+            val vcode = lr.optInt("versionCode", 0)
+            val dl = lr.optString("downloadUrl", "")
+            if (vcode > 0 && dl.isNotBlank()) {
+                result[id] = RepoSummary(ver, vcode, dl)
+            }
+        }
+        return result
+    }
+}
+
+data class ModuleDetail(
+    val readme: String?,
+    val latestTag: String,
+    val latestTime: String,
+    val latestAssetName: String?,
+    val latestAssetUrl: String?,
+    val releases: List<ReleaseInfo>
+)
+
+data class ReleaseInfo(
+    val name: String,
+    val tagName: String,
+    val publishedAt: String,
+    val descriptionHTML: String,
+    val assets: List<ReleaseAssetInfo>
+)
+
+data class ReleaseAssetInfo(
+    val name: String,
+    val downloadUrl: String,
+    val size: Long
+)
+
+fun fetchReleaseDescriptionHtml(moduleId: String, latestTag: String): String? {
+    val url = "https://modules.kernelsu.org/module/$moduleId.json"
+    ksuApp.okhttpClient.newCall(Request.Builder().url(url).build()).execute().use { resp ->
+        if (!resp.isSuccessful) return null
+        val body = resp.body?.string() ?: return null
+        val obj = JSONObject(body)
+        val releasesArray = obj.optJSONArray("releases") ?: return null
+        var fallbackHtml: String? = null
+        for (i in 0 until releasesArray.length()) {
+            val r = releasesArray.optJSONObject(i) ?: continue
+            val descHtml = r.optString("descriptionHTML", "")
+            if (fallbackHtml == null && descHtml.isNotBlank()) {
+                fallbackHtml = descHtml
+            }
+            val rname = r.optString("name", r.optString("tagName", r.optString("version", "")))
+            if (rname == latestTag && descHtml.isNotBlank()) {
+                return descHtml
+            }
+        }
+        return fallbackHtml
+    }
+}
+
+fun fetchModuleDetail(moduleId: String): ModuleDetail? {
+    val url = "https://modules.kernelsu.org/module/$moduleId.json"
+    ksuApp.okhttpClient.newCall(Request.Builder().url(url).build()).execute().use { resp ->
+        if (!resp.isSuccessful) return null
+        val body = resp.body?.string() ?: return null
+        val obj = JSONObject(body)
+
+        val readme = obj.optString("readme", "").ifBlank { null }
+    val lr = obj.optJSONObject("latestRelease")
+    var latestTag = ""
+    var latestTime = ""
+    var latestAssetName: String? = null
+    var latestAssetUrl: String? = null
+    if (lr != null) {
+        latestTag = lr.optString("name", lr.optString("version", ""))
+        latestTime = lr.optString("time", "")
+        val urlDl = lr.optString("downloadUrl", "")
+        if (urlDl.isNotEmpty()) {
+            latestAssetName = urlDl.substringAfterLast('/')
+            latestAssetUrl = urlDl
+        }
+    } else {
+        // fallback: schema may provide latest release as a string
+        latestTag = obj.optString("latestRelease", "")
+    }
+
+        val releasesArray = obj.optJSONArray("releases")
+        val releases = if (releasesArray != null) {
+            (0 until releasesArray.length()).mapNotNull { rIdx ->
+                val r = releasesArray.optJSONObject(rIdx) ?: return@mapNotNull null
+                val rname = r.optString("name", "")
+                val publishedAt = r.optString("publishedAt", "")
+                val descHtml = r.optString("descriptionHTML", "")
+                val assetsArray = r.optJSONArray("releaseAssets") ?: JSONArray()
+                val assets = (0 until assetsArray.length()).mapNotNull { aIdx ->
+                    val a = assetsArray.optJSONObject(aIdx) ?: return@mapNotNull null
+                    val aname = a.optString("name", "")
+                    val adl = a.optString("downloadUrl", "")
+                    val asz = a.optLong("size", 0L)
+                    if (aname.isEmpty() || adl.isEmpty()) null else ReleaseAssetInfo(aname, adl, asz)
+                }
+                ReleaseInfo(
+                    name = rname,
+                    tagName = rname,
+                    publishedAt = publishedAt,
+                    descriptionHTML = descHtml,
+                    assets = assets
+                )
+            }
+        } else emptyList()
+
+        return ModuleDetail(
+            readme = readme,
+            latestTag = latestTag,
+            latestTime = latestTime,
+            latestAssetName = latestAssetName,
+            latestAssetUrl = latestAssetUrl,
+            releases = releases
+        )
+    }
+}

manager/app/src/main/java/com/sukisu/ultra/ui/util/module/VersionCompare.kt
@@ -1,0 +1,20 @@
+package com.sukisu.ultra.ui.util.module
+
+enum class UpdateState {
+    CAN_UPDATE,
+    EQUAL,
+    OLDER,
+    NOT_INSTALLED,
+    UNKNOWN
+}
+
+fun compareVersionCode(installedCode: Int?, repoCode: Int): UpdateState {
+    if (repoCode <= 0) return UpdateState.UNKNOWN
+    val ic = installedCode ?: return UpdateState.NOT_INSTALLED
+    return when {
+        repoCode > ic -> UpdateState.CAN_UPDATE
+        repoCode == ic -> UpdateState.EQUAL
+        else -> UpdateState.OLDER
+    }
+}
+

manager/app/src/main/java/com/sukisu/ultra/ui/viewmodel/ModuleViewModel.kt
@@ -24,6 +24,9 @@ import com.sukisu.ultra.ksuApp
 import com.sukisu.ultra.ui.component.SearchStatus
 import com.sukisu.ultra.ui.util.HanziToPinyin
 import com.sukisu.ultra.ui.util.listModules
+import com.sukisu.ultra.ui.util.module.RepoSummary
+import com.sukisu.ultra.ui.util.module.fetchRepoIndex
+import com.sukisu.ultra.ui.util.module.sanitizeVersionString
 import org.json.JSONArray
 import org.json.JSONObject
 import java.text.Collator
@@ -157,6 +160,7 @@ class ModuleViewModel : ViewModel() {
                         executable -> 3
                         else -> 4
                     }
+
                     sortEnabledFirst && !sortActionFirst -> if (it.enabled) 1 else 2
                     !sortEnabledFirst && sortActionFirst -> if (executable) 1 else 2
                     else -> 1
@@ -226,49 +230,16 @@ class ModuleViewModel : ViewModel() {
         }
     }
 
-    @Immutable
-    data class RepoSummary(
-        val latestVersion: String,
-        val versionCode: Int,
-        val downloadUrl: String
-    )
-
     private val _repoIndex = mutableStateMapOf<String, RepoSummary>()
-    val repoIndex: SnapshotStateMap<String, RepoSummary> = _repoIndex
 
     suspend fun refreshRepoIndex() {
-        val parsed = withContext(Dispatchers.IO) {
-            kotlin.runCatching {
-                val req = okhttp3.Request.Builder().url("https://modules.kernelsu.org/modules.json").build()
-                ksuApp.okhttpClient.newCall(req).execute().use { resp ->
-                    if (!resp.isSuccessful) emptyList() else {
-                        val body = resp.body?.string() ?: return@use emptyList()
-                        val arr = org.json.JSONArray(body)
-                        (0 until arr.length()).mapNotNull { idx ->
-                            val obj = arr.optJSONObject(idx) ?: return@mapNotNull null
-                            val id = obj.optString("moduleId", "").ifBlank { return@mapNotNull null }
-                            val lr = obj.optJSONObject("latestRelease")
-                            if (lr == null) null else {
-                                val ver = sanitizeVersionString(lr.optString("name", lr.optString("version", "")))
-                                val vcode = lr.optInt("versionCode", 0)
-                                val dl = lr.optString("downloadUrl", "")
-                                if (vcode <= 0 || dl.isBlank()) null else id to RepoSummary(ver, vcode, dl)
-                            }
-                        }
-                    }
-                }
-            }.getOrElse { emptyList() }
-        }
-
+        val parsed = withContext(Dispatchers.IO) { fetchRepoIndex() }
         withContext(Dispatchers.Main) {
             _repoIndex.clear()
             parsed.forEach { (id, summary) -> _repoIndex[id] = summary }
         }
     }
 
-    private fun sanitizeVersionString(version: String): String {
-        return version.replace(Regex("[^a-zA-Z0-9.\\-_]"), "_")
-    }
 
     private fun ModuleInfo.toSignature(): ModuleUpdateSignature {
         return ModuleUpdateSignature(