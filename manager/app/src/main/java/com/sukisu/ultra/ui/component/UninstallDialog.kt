tiann/KernelSU

manager: Migrate to navigation3 + navigationevent (#3164)
This pull request migrates the app's navigation system from the
`compose-destinations` library to the new `androidx.navigation3` and
`navigationevent` libraries, modernizing navigation and simplifying
related code. Removes now-unnecessary KSP
(Kotlin Symbol Processing) configuration.

manager/app/src/main/java/me/weishu/kernelsu/ui/component/UninstallDialog.kt
@@ -15,9 +15,9 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
-import com.ramcosta.composedestinations.generated.destinations.FlashScreenDestination
 import me.weishu.kernelsu.R
-import me.weishu.kernelsu.ui.component.navigation.MiuixDestinationsNavigator
+import me.weishu.kernelsu.ui.navigation3.Navigator
+import me.weishu.kernelsu.ui.navigation3.Route
 import me.weishu.kernelsu.ui.screen.FlashIt
 import me.weishu.kernelsu.ui.screen.UninstallType
 import me.weishu.kernelsu.ui.screen.UninstallType.NONE
@@ -34,7 +34,7 @@ import top.yukonga.miuix.kmp.theme.MiuixTheme
 @Composable
 fun UninstallDialog(
     showDialog: MutableState<Boolean>,
-    navigator: MiuixDestinationsNavigator,
+    navigator: Navigator,
 ) {
     val context = LocalContext.current
     val options = listOf(
@@ -50,19 +50,9 @@ fun UninstallDialog(
 
     val run = { type: UninstallType ->
         when (type) {
-            PERMANENT -> navigator.navigate(FlashScreenDestination(FlashIt.FlashUninstall)) {
-                popUpTo(FlashScreenDestination) {
-                    inclusive = true
-                }
-                launchSingleTop = true
-            }
+            PERMANENT -> navigator.push(Route.Flash(FlashIt.FlashUninstall))
 
-            RESTORE_STOCK_IMAGE -> navigator.navigate(FlashScreenDestination(FlashIt.FlashRestore)) {
-                popUpTo(FlashScreenDestination) {
-                    inclusive = true
-                }
-                launchSingleTop = true
-            }
+            RESTORE_STOCK_IMAGE -> navigator.push(Route.Flash(FlashIt.FlashRestore))
 
             TEMPORARY -> showTodo()
             NONE -> Unit

manager/app/src/main/java/me/weishu/kernelsu/ui/screen/Flash.kt
@@ -4,7 +4,6 @@ import android.net.Uri
 import android.os.Environment
 import android.os.Parcelable
 import android.widget.Toast
-import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -47,15 +46,13 @@ import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.dropUnlessResumed
-import com.ramcosta.composedestinations.annotation.Destination
-import com.ramcosta.composedestinations.annotation.RootGraph
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import kotlinx.parcelize.Parcelize
 import me.weishu.kernelsu.R
 import me.weishu.kernelsu.ui.component.KeyEventBlocker
-import me.weishu.kernelsu.ui.component.navigation.MiuixDestinationsNavigator
+import me.weishu.kernelsu.ui.navigation3.Navigator
 import me.weishu.kernelsu.ui.util.FlashResult
 import me.weishu.kernelsu.ui.util.LkmSelection
 import me.weishu.kernelsu.ui.util.flashModule
@@ -107,9 +104,8 @@ fun flashModulesSequentially(
 }
 
 @Composable
-@Destination<RootGraph>
 fun FlashScreen(
-    navigator: MiuixDestinationsNavigator,
+    navigator: Navigator,
     flashIt: FlashIt
 ) {
     var text by rememberSaveable { mutableStateOf("") }
@@ -152,14 +148,11 @@ fun FlashScreen(
         }
     }
 
-    BackHandler {
-        navigator.popBackStack()
-    }
     Scaffold(
         topBar = {
             TopBar(
                 flashing,
-                onBack = dropUnlessResumed { navigator.popBackStack() },
+                onBack = dropUnlessResumed { navigator.pop() },
                 onSave = {
                     scope.launch {
                         val format = SimpleDateFormat("yyyy-MM-dd-HH-mm-ss", Locale.getDefault())

s78910/SukiSU-Ultra

Co-authored-by: ShirkNeko <109797057+ShirkNeko@users.noreply.github.com>

manager/app/src/main/java/com/sukisu/ultra/ui/component/UninstallDialog.kt
@@ -15,9 +15,9 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
-import com.ramcosta.composedestinations.generated.destinations.FlashScreenDestination
 import com.sukisu.ultra.R
-import com.sukisu.ultra.ui.component.navigation.MiuixDestinationsNavigator
+import com.sukisu.ultra.ui.navigation3.Navigator
+import com.sukisu.ultra.ui.navigation3.Route
 import com.sukisu.ultra.ui.screen.FlashIt
 import com.sukisu.ultra.ui.screen.UninstallType
 import com.sukisu.ultra.ui.screen.UninstallType.NONE
@@ -34,7 +34,7 @@ import top.yukonga.miuix.kmp.theme.MiuixTheme
 @Composable
 fun UninstallDialog(
     showDialog: MutableState<Boolean>,
-    navigator: MiuixDestinationsNavigator,
+    navigator: Navigator,
 ) {
     val context = LocalContext.current
     val options = listOf(
@@ -50,19 +50,9 @@ fun UninstallDialog(
 
     val run = { type: UninstallType ->
         when (type) {
-            PERMANENT -> navigator.navigate(FlashScreenDestination(FlashIt.FlashUninstall)) {
-                popUpTo(FlashScreenDestination) {
-                    inclusive = true
-                }
-                launchSingleTop = true
-            }
+            PERMANENT -> navigator.push(Route.Flash(FlashIt.FlashUninstall))
 
-            RESTORE_STOCK_IMAGE -> navigator.navigate(FlashScreenDestination(FlashIt.FlashRestore)) {
-                popUpTo(FlashScreenDestination) {
-                    inclusive = true
-                }
-                launchSingleTop = true
-            }
+            RESTORE_STOCK_IMAGE -> navigator.push(Route.Flash(FlashIt.FlashRestore))
 
             TEMPORARY -> showTodo()
             NONE -> Unit

manager/app/src/main/java/com/sukisu/ultra/ui/screen/Flash.kt
@@ -5,7 +5,6 @@ import android.net.Uri
 import android.os.Environment
 import android.os.Parcelable
 import android.widget.Toast
-import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -50,15 +49,13 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.dropUnlessResumed
 import kotlinx.coroutines.delay
-import com.ramcosta.composedestinations.annotation.Destination
-import com.ramcosta.composedestinations.annotation.RootGraph
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import kotlinx.parcelize.Parcelize
 import com.sukisu.ultra.R
 import com.sukisu.ultra.ui.component.KeyEventBlocker
-import com.sukisu.ultra.ui.component.navigation.MiuixDestinationsNavigator
+import com.sukisu.ultra.ui.navigation3.Navigator
 import com.sukisu.ultra.ui.util.FlashResult
 import com.sukisu.ultra.ui.util.LkmSelection
 import com.sukisu.ultra.ui.util.flashModule
@@ -110,9 +107,8 @@ fun flashModulesSequentially(
 }
 
 @Composable
-@Destination<RootGraph>
 fun FlashScreen(
-    navigator: MiuixDestinationsNavigator,
+    navigator: Navigator,
     flashIt: FlashIt
 ) {
     var text by rememberSaveable { mutableStateOf("") }
@@ -173,14 +169,11 @@ fun FlashScreen(
         }
     }
 
-    BackHandler {
-        navigator.popBackStack()
-    }
     Scaffold(
         topBar = {
             TopBar(
                 flashing,
-                onBack = dropUnlessResumed { navigator.popBackStack() },
+                onBack = dropUnlessResumed { navigator.pop() },
                 onSave = {
                     scope.launch {
                         val format = SimpleDateFormat("yyyy-MM-dd-HH-mm-ss", Locale.getDefault())