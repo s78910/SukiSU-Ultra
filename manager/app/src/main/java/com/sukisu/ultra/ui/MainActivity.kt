ca446da102ed9386fbd5afddfd2d7bf5c84ebe77
ShirkNeko <109797057+ShirkNeko@users.noreply.github.com>
2026-01-21 00:40:28 +0800

    manager: Reintroduce the external installer omitted during cherry-pick

--- manager/app/src/main/java/com/sukisu/ultra/ui/MainActivity.kt
+++ manager/app/src/main/java/com/sukisu/ultra/ui/MainActivity.kt
@@ -198,6 +198,7 @@ class MainActivity : ComponentActivity() {
                         }
                     )
                 }
+                HandleZipFileIntent(intent, navigator)
             }
         }
     }
